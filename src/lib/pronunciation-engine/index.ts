76060e3a8b4c05b8d6a8ec457b4797dece87a672
Thunderkill <66909862+Thunderkill016@users.noreply.github.com>
2026-09-04 02:08:59 +0700

    chore: export canonical CTC lattice

--- src/lib/pronunciation-engine/index.ts
+++ src/lib/pronunciation-engine/index.ts
@@ -3,6 +3,7 @@ export * from "./assessment";
 export * from "./benchmark";
 export * from "./calibration";
 export * from "./ctc";
+export * from "./ctc-lattice";
 export * from "./evidence";
 export * from "./metrics";
 export * from "./operating-point";
